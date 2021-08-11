0a9e9788a9338c310112386debe1df0137f6b547
PalomaReb <paloma.rebolleda@gmail.com>
2021-08-11 20:36:24 +0200

    componentes divididos

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,34 +1,12 @@
-import imagen from '../src/assets/foto.png'
 import './App.css';
+import SmallCard from './components/larger-container/small-container'
+import LargeContainer from './components/larger-container/larger-container'
 
 function App() {
   return (
     <div className="container">
-      
-      <aside className="sideCard-container">
-        <input className="inp-city" placeholder=" 🔍 Search for Places"></input>
-        <img className="imgStyle" src={imagen} alt={"imagen"}></img>
-        <p className="big-number">12 c</p>
-        <p>Monday</p>
-        <p>Mostly cloudy</p>
-        <p>Rainy</p>
-      </aside>
-
-      <section className="card-container">
-
-        <div className="card-header">
-        <h2>Week <hr></hr></h2>
-
-        <div className="btn-container">
-        <button className="btn-c">C</button>
-        <button className="btn-f">F</button>
-        </div>
-
-        </div>
-        <p> ksdnwlkfnñkadwlfñlkadjfñlkdsjflkdjlkdj´lkdsjg´lkdsjg´lkdsjg´lkdsjgkl´dsjgjgj </p>
-      <h2>Today's Highlights</h2>
-      </section>
-      
+      <SmallCard></SmallCard>
+      <LargeContainer></LargeContainer>
     </div>
   );
 }
---
+++ my-app/src/components/larger-container/small-container/index.jsx
@@ -0,0 +1,17 @@
+import "./styles.css";
+import imagen from "../../../assets/foto.png";
+
+function SmallCard() {
+  return (
+    <aside className="sideCard-container">
+      <input className="inp-city" placeholder=" 🔍 Search for Places"></input>
+      <img className="imgStyle" src={imagen} alt={"imagen"}></img>
+      <p className="big-number">12 c</p>
+      <p>Monday</p>
+      <p>Mostly cloudy</p>
+      <p>Rainy</p>
+    </aside>
+  );
+}
+
+export default SmallCard;
