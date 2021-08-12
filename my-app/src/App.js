684f1623591d995ec09c0e4d0b61dbfd16542ebe
jose <jose.tovarcabrera@gmail.com>
2021-08-12 19:08:28 +0200

    no message

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,45 +1,15 @@
 import './App.css';
-<<<<<<< HEAD
-import Fetch from './fetchPrueba';
-=======
 import SmallCard from './components/larger-container/small-container'
 import LargeContainer from './components/larger-container/larger-container'
->>>>>>> 2af105aeec516a9b1be808c71b48fc28dfde8df6
+import Fetch from './fetchPrueba';
 
 function App() {
   return (
     <div className="container">
-<<<<<<< HEAD
-      
-      <aside className="sideCard-container">
-        <input className="inp-city" placeholder=" 🔍 Search for Places"></input>
-        <img className="imgStyle" src={imagen} alt={"imagen"}></img>
-        <p className="big-number">12 c</p>
-        <p>Monday</p>
-        <p>Mostly cloudy</p>
-        <p>Rainy</p>
-      </aside>
-
-      <section className="card-container">
-
-        <div className="card-header">
-        <h2>Week <hr></hr></h2>
-
-        <div className="btn-container">
-        <button className="btn-c">C</button>
-        <button className="btn-f">F</button>
-        </div>
-
-        </div>
-        <p> ksdnwlkfnñkadwlfñlkadjfñlkdsjflkdjlkdj´lkdsjg´lkdsjg´lkdsjg´lkdsjgkl´dsjgjgj </p>
-      <h2>Today's Highlights</h2>
-      <Fetch></Fetch>
-      </section>
-      
-=======
       <SmallCard></SmallCard>
       <LargeContainer></LargeContainer>
->>>>>>> 2af105aeec516a9b1be808c71b48fc28dfde8df6
+      <Fetch></Fetch>
+
     </div>
   );
 }
--- my-app/src/fetchPrueba.jsx
+++ my-app/src/fetchPrueba.jsx
@@ -3,16 +3,20 @@ import  {WeatherKey} from './keys/keys.js'
 
 
 function Fetch (){
-    const [clima,setClima]=useState([""])
+    const [clima,setClima]=useState()
+    fetch( `http://openweathermap.org/img/wn/10d@2x.png`)
+    .then(response=> response.json())
+    .then(data=> setClima(data))
 
 return <div>
-    <input onClick={(e)=>{ 
+    {/* <input   onClick={(e)=>{ 
+        e.preventDefault()
         const ciudad=e.target.value;
         const Apiurl=`https://api.openweathermap.org/data/2.5/weather?q=${ciudad}&appid=${ WeatherKey}`;
         fetch(Apiurl)
         .then(response => response.json())
-        .then(data => setClima(data.clouds.all));
-    }} />
+        .then(data => console.log(data.weather[0].icon));
+    }} /> */}
     <input onClick={(e)=>{ 
         
         const Apiurl2=`https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid=${ WeatherKey}`;
@@ -20,7 +24,7 @@ return <div>
         .then(response => response.json())
         .then(data => console.log(data));
     }} />
-    <p>{clima}</p>
+   <img src={clima} alt="" /> 
 </div>
 }
 
